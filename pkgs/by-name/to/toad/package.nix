2853c058b5b5ccfda0f52454a5a97693de9d50e1
Hannes <h@nnes.dev>
2026-01-22 20:36:23 +0800

    toad again again

--- pkgs/by-name/to/toad/package.nix
+++ pkgs/by-name/to/toad/package.nix
@@ -1,21 +1,20 @@
 {
   lib,
-
+  python3Packages,
   fetchFromGitHub,
-  python3,
   versionCheckHook,
 }:
 
-python3.pkgs.buildPythonApplication (finalAttrs: {
+python3Packages.buildPythonApplication (finalAttrs: {
   pname = "toad";
-  version = "0.6.9";
+  version = "0.5.38";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "batrachianai";
     repo = "toad";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-lBgYhF/bV3Ogo6HzyF+ykHB6P5cEQDXtzSCQl0MErkc=";
+    hash = "";
   };
 
   # Fix circular import problem with type-hints
@@ -23,16 +22,15 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     sed -i '1i from __future__ import annotations' src/toad/app.py
   '';
 
-  build-system = [ python3.pkgs.hatchling ];
+  build-system = with python3Packages; [ hatchling ];
 
   dependencies =
-    with python3.pkgs;
+    with python3Packages;
     [
-      aiosqlite
       bashlex
       click
       gitpython
-      # google-re2
+      google-re2
       httpx
       notify-py
       packaging
@@ -52,16 +50,22 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     ]
     ++ textual.optional-dependencies.syntax;
 
-  # pythonRelaxDeps=["google-re2" "notify-py" "psutil"];
+  pythonRelaxDeps = [
+    "google-re2"
+    "notify-py"
+    "psutil"
+  ];
 
-  pythonImportsCheck = [ "toad" ];
+  pythonImportsCheck = [
+    "toad"
+  ];
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgramArg = "--version";
 
   meta = {
-    description = "A unified experience for AI in your terminal";
+    description = "A unified interface for AI in your terminal";
     homepage = "https://github.com/batrachianai/toad";
     changelog = "https://github.com/batrachianai/toad/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.agpl3Only;
